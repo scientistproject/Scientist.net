f71e377530c9c760d8abbef200078aab51fb0638
joncloud <jdberube@gmail.com>
2016-02-04 20:31:50 -0800

    Improved XML documentation

--- src/Scientist/IExperiment.cs
+++ src/Scientist/IExperiment.cs
@@ -3,17 +3,41 @@ using System.Threading.Tasks;
 
 namespace GitHub.Internals
 {
+    /// <summary>
+    /// Provides an interface for defining a synchronous experiment.
+    /// </summary>
+    /// <typeparam name="T">The return result for the experiment.</typeparam>
     public interface IExperiment<T>
     {
+        /// <summary>
+        /// Defines the operation to try.
+        /// </summary>
+        /// <param name="candidate">The delegate to execute.</param>
         void Try(Func<T> candidate);
 
+        /// <summary>
+        /// Defines the operation to actually use.
+        /// </summary>
+        /// <param name="control">The delegate to execute.</param>
         void Use(Func<T> control);
     }
 
+    /// <summary>
+    /// Provides an interface for defining an asynchronous experiment.
+    /// </summary>
+    /// <typeparam name="T">The return result for the experiment.</typeparam>
     public interface IExperimentAsync<T>
     {
+        /// <summary>
+        /// Defines the operation to try.
+        /// </summary>
+        /// <param name="candidate">The delegate to execute.</param>
         void Try(Func<Task<T>> candidate);
 
+        /// <summary>
+        /// Defines the operation to actually use.
+        /// </summary>
+        /// <param name="control">The delegate to execute.</param>
         void Use(Func<Task<T>> control);
     }
 
