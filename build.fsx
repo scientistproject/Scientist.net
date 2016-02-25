a3ab1bf246af478b23aab76bc481aa174463877d
Dave Zych <david.zych@dptechnology.com>
2016-02-25 15:58:03 -0800

    Clean spaces

--- build.fsx
+++ build.fsx
@@ -27,53 +27,53 @@ let Run workingDirectory fileName args =
     let errors = new List<string>()
     let messages = new List<string>()
     let timout = TimeSpan.MaxValue
-        
+
     let error msg =
         traceError msg
         errors.Add msg
-        
+
     let message msg =
         traceImportant msg
         messages.Add msg
-        
+
     let code = 
         ExecProcessWithLambdas (fun info ->
             info.FileName <- fileName
             info.WorkingDirectory <- workingDirectory
             info.Arguments <- args
         ) timout true error message
-    
+
     ProcessResult.New code messages errors
-    
+
 let GetHomeDirectory =
     let result = Run currentDirectory "cmd" "/c \"echo %USERPROFILE%\""
     result.Messages.[0]
-    
+
 let GetDnvmHome =
     let homeDirectory =
         if buildServer = BuildServer.AppVeyor    
             then "C:\Program Files (x86)\MSBuild\14.0\Bin\\"
             else (GetHomeDirectory + "\\.dnx\\bin\\")
-    
+
     homeDirectory
-    
-let GetDnxHome = 
+
+let GetDnxHome =
     let homeDirectory = GetHomeDirectory
     homeDirectory + "\\.dnx\\runtimes\\dnx-clr-win-x86.1.0.0-rc1-update1\\bin\\"
-    
+
 let UpdateProjectJson projectJson =
-    let fullJsonPath = (__SOURCE_DIRECTORY__ + projectJson)    
+    let fullJsonPath = (__SOURCE_DIRECTORY__ + projectJson)
     let backupJsonPath = (fullJsonPath + ".bak")
-    
+
     CopyFile backupJsonPath fullJsonPath
-    
+
     let tempReleaseNotes = "Temporary release notes\\nWith new line"
     RegexReplaceInFileWithEncoding "\"releaseNotes\": \"\"," ("\"releaseNotes\": \"" + tempReleaseNotes +  "\",") Encoding.UTF8 fullJsonPath
-    
-let RestoreProjectJson projectJson = 
-    let fullJsonPath = (__SOURCE_DIRECTORY__ + projectJson)    
+
+let RestoreProjectJson projectJson =
+    let fullJsonPath = (__SOURCE_DIRECTORY__ + projectJson)
     let backupJsonPath = (fullJsonPath + ".bak")
-    
+
     DeleteFile fullJsonPath
     CopyFile fullJsonPath backupJsonPath
     DeleteFile backupJsonPath
@@ -89,9 +89,9 @@ Target "Clean" (fun _ ->
 
 Target "SetupBuild" (fun _ ->
     DnxHome <- GetDnxHome
-    
+
     SetDnxBuildVersion
-    
+
     let dnvmHome = GetDnvmHome
     Run currentDirectory (dnvmHome + "dnvm.cmd") "install 1.0.0-rc1-update1 -r clr -a x86" |> ignore
     Run currentDirectory (dnvmHome + "dnvm.cmd") "use 1.0.0-rc1-update1 -r clr -a x86" |> ignore
@@ -102,13 +102,13 @@ Target "BuildApp" (fun _ ->
     Run currentDirectory (DnxHome + "dnu.cmd") "build .\\test\\Scientist.Test\\ --configuration Release" |> ignore
 )
 
-Target "CreatePackages" (fun _ -> 
+Target "CreatePackages" (fun _ ->
     let scientistJsonPath = "/src/Scientist/project.json"
-    
+
     UpdateProjectJson scientistJsonPath
-    
+
     Run currentDirectory (DnxHome + "dnu.cmd") ("pack .\\src\\Scientist\\ --configuration Release --out " + packagingDir) |> ignore
-    
+
     RestoreProjectJson scientistJsonPath
 )
 
@@ -120,16 +120,16 @@ Target "Default" DoNothing
 
 "Clean"
     ==> "SetupBuild"
-    
-"SetupBuild" 
+
+"SetupBuild"
     ==> "BuildApp"
-    
+
 "SetupBuild"
     ==> "RunTests"
-    
+
 "SetupBuild"
     ==> "CreatePackages"
-    
+
 "RunTests"
     ==> "Default"
 
