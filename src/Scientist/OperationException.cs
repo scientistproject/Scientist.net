a12b92a0e73a9b072d2fa35887062fd333b9608d
Mark Lopez <m@silvenga.com>
2017-07-12 18:52:44 -0500

    Created OperationException.

---
+++ src/Scientist/OperationException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GitHub
+{
+    /// <summary>
+    /// Thrown when an exception was thrown during the execution of an operation.
+    /// </summary>
+    public class OperationException : Exception
+    {
+        /// <summary>
+        /// The operation in which this exception occurred within.
+        /// </summary>
+        public Operation Operation { get; }
+
+        public override string Message { get; }
+
+        internal OperationException(Operation operation, Exception inner) : base(null, inner)
+        {
+            Operation = operation;
+            Message = GetExceptionMessage(operation);
+        }
+
+        private static string GetExceptionMessage(Operation operation)
+        {
+            string exceptionMessage = Enum.IsDefined(typeof(Operation), operation)
+                ? $"An exception occurred within the experiment during the '{operation}' operation. Consider checking both the inner exception and the {nameof(Operation)} property for details on this exception."
+                : "An exception occurred within the experiment, the operation was unknown. The operation should never be unknown, please report this.";
+            return exceptionMessage;
+        }
+    }
+}
